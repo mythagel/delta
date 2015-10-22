7048bb9038dcf5ae6d4fe49a12ad1186ae6085f2
Nicholas Gill <mythagel@gmail.com>
2015-10-22 23:47:46 +1100

    Implement back face ops

---
+++ beta3/cam/motor_mount.op2.lua
@@ -0,0 +1,11 @@
+require("common")
+local cam = require("cam")
+
+-- stock rotated to machine from back face
+cam.tool(18); tool_r = 8/2;
+
+move_to(42/2, 42/2, 0);
+for _ = 1, 2 do
+    cam.plunge(1, 20)
+    cam.polygon((22/2) - tool_r, 64, 64, true, 50)
+end
