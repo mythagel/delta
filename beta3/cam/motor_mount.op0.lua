8aeba0bd44b2e328642d636bf57661a6c823fa53
Nicholas Gill <mythagel@gmail.com>
2015-10-23 22:04:14 +1100

    Use Troichoidal slotting function

--- beta3/cam/motor_mount.op0.lua
+++ beta3/cam/motor_mount.op0.lua
@@ -23,17 +23,20 @@ for i = 1, 4 do
 end
 
 cam.tool(18); tool_r = 8/2;
-move_to(42/2, 42/2, 0);
+move_to(nil, nil, 1);
+move_to(42/2, 42/2, nil);
+move_to(nil, nil, 0);
 cam.helical_plunge((16/2) - tool_r, d+1, 0.5, 50);
 move_to(nil, nil, 0);
 
 turn_to(90)
-move_to((42/2)-((16/2)-tool_r), 0, nil)
+move_to(nil, nil, 1);
+move_to((42/2)-((16/2)-tool_r), -tool_r-1, nil)
+move_to(nil, nil, 0);
+cam.plunge(7, 10);
+cam.trochoidal_slot((42/2)+1, 1, (16/2)-tool_r, 200)
 
-for i = 1, 7 do
-    cam.rectangle((42/2), 16-(tool_r*2), -1, 50);
-    if i < 7 then
-        cam.plunge(1, 50)
-    end
-end
+-- finish pass
+move_to((42/2)-((16/2)-tool_r), 0, nil)
+cam.rectangle((42/2), 16-(tool_r*2), -1, 50);
 
