b2e3a71944f6a26c85e123e5334e1b689d2b3552
Nicholas Gill <mythagel@gmail.com>
2016-02-08 22:50:52 +1100

    Adapt for design changes

--- beta3/cam/motor_mount.op1.lua
+++ beta3/cam/motor_mount.op1.lua
@@ -4,9 +4,11 @@ local cam = require("cam")
 -- stock rotated to machine from base
 cam.tool(8)
 
-move_to(0+((15/2)+1), d/2, 0);
+bearing_radius = 10/2;
+
+move_to(0+(bearing_radius+1), d/2, 0);
 cam.peck_drill(20, 1, 50);
 
-move_to(w-((15/2)+1), d/2, 0);
+move_to(w-(bearing_radius+1), d/2, 0);
 cam.peck_drill(20, 1, 50);
 
