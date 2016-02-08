6e7567b95db08cad89ceb3e8d9ef77fb455cde0e
Nicholas Gill <mythagel@gmail.com>
2016-02-08 22:51:14 +1100

    Don't move at z0

--- beta3/cam/motor_mount.op2.lua
+++ beta3/cam/motor_mount.op2.lua
@@ -4,7 +4,9 @@ local cam = require("cam")
 -- stock rotated to machine from back face
 cam.tool(18); tool_r = 8/2;
 
-move_to(42/2, 42/2, 0);
+move_to(nil, nil, 1);
+move_to(42/2, 42/2, nil);
+cam.plunge(1, 20);
 for _ = 1, 2 do
     cam.plunge(1, 20)
     cam.polygon((22/2) - tool_r, 64, 64, true, 50)
