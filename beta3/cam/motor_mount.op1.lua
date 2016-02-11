ba192f61cae7d71d2840935f6cca788d249edc53
Nicholas Gill <mythagel@gmail.com>
2016-02-11 23:09:02 +1100

    Don't move along part + minor build fixes

--- beta3/cam/motor_mount.op1.lua
+++ beta3/cam/motor_mount.op1.lua
@@ -6,9 +6,13 @@ cam.tool(8)
 
 bearing_radius = 10/2;
 
-move_to(0+(bearing_radius+1), d/2, 0);
+move_to(nil, nil, 1)
+
+move_to(0+(bearing_radius+1), d/2, nil);
+move_to(nil, nil, 0)
 cam.peck_drill(20, 1, 50);
 
-move_to(w-(bearing_radius+1), d/2, 0);
+move_to(w-(bearing_radius+1), d/2, nil);
+move_to(nil, nil, 0)
 cam.peck_drill(20, 1, 50);
 
