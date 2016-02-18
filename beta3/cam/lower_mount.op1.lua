2f4b5d018b1db6d5f02aca51487a44fd6af0e9c9
Nicholas Gill <mythagel@gmail.com>
2016-02-18 23:14:58 +1100

    Add bearing mount

--- beta3/cam/lower_mount.op1.lua
+++ beta3/cam/lower_mount.op1.lua
@@ -1,10 +1,14 @@
 require("common")
 local cam = require("cam")
 
+function retract_to(z)
+    move_to(nil, nil, z)
+end
+
 -- stock rotated to machine from top
 cam.tool(14); tool_r = 3.9875/2;
 
-move_to(nil, nil, 1)
+retract_to(1)
 
 move_to((w/2 - 16/2)+tool_r, 24+tool_r, nil)
 move_to(nil, nil, 0)
@@ -18,3 +22,10 @@ pocket_w = 16-tool_r;
 cam.rectangle(pocket_w, pocket_d, 1, 20);
 cam.square_zag(pocket_d/6, pocket_w, 6, 1, 20)
 --turn(90)
+
+
+retract_to(1)
+cam.tool(2); tool_r = 1;
+move_to(w/2, 24-(16/2), nil)
+cam.peck_drill(19, 1, 50);
+
